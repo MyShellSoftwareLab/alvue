ef3344da7315b040638c4db264fadf8da310fe76
Guss Rw <GussRw1@Gmail.com>
2020-10-23 13:16:23 -0500

    Merge for release (#8)
    * README has been added
    
    * getFormObject function was optimized with recursion methods
    
    * Version of package was updated
    
    * dist folder was updated with build files
    
    * disable button and spinner was added
    
    * keyword of plugin was updated
    
    * version of plugin was updated
    
    * spinner and disable submit button was added
    
    * Was added the error class for the parents input
    
    * version updated
    
    * New prop for set axios config (#4)
    
    New prop for html errors support
    
    * Feature/focus input (#6)
    
    * focus input error was added
    
    * focus input error was added
    
    * form method was updated to post or get (#7)
    
    Co-authored-by: Jorge Solis <jorge.solis@cceo.com.mx>

---
+++ README.md
@@ -0,0 +1,51 @@
+<h2 align="center">ALVue - Ajax Laravel Validator for Vue</h2>
+    
+ALVue is the update of the library that previously worked with JQuery, 
+this new version includes a Vue component that will help you show the errors of a Laravel FormRequest in its forms.
+
+
+#### Installation
+
+```shell    
+npm install @myshell/alvue --save
+```    
+
+#### Import
+```js
+import alvue from '@myshell/alvue'
+Vue.use(alvue);
+```
+
+#### Usage
+```html    
+<alv-form action="/example-url" method="post" @after-done="reloadPage" :data-object="user">
+    <div>
+        <label>Name </label>
+        <input type="text" name="name" v-model="user.name">
+     </div>
+    ...
+    <button type="submit">Save</button>
+</alv-form>
+```
+
+
+### Properties
+
+| Name                   | Type     | Required | Default     | Description                         |
+| ---                    | ---      |   ---    | ---         | ---                                 |
+| action                 | String   |  true    | undefined   | URL to post.                        |
+| method                 | String   |  false   | post        | HTTP method used to send data.      |
+| data-object            | Object   |  false   | undefined   | Object to send sent to server <br> If it is not included, form data is sent by input name.     |
+| input-parent-selector  | String   |  false   | div         | Parent of input to append error.    |
+| spinner                | Boolean  |  false   | false       | Append loading spinner.             |
+| error-class            | String   |  false   | null        | Parent of input to append error class. |
+| axios-config           | Object   |  false   | {}          | Axios config to create the instance. |
+| html-errors            | Boolean  |  false   | false       | Support for displaying errors with html. |
+
+
+### Events
+
+| Name        | Description              |
+| ---         | ---                      |
+| after-done  | Triggered after the server response is successful. <br> Contains response payload.|
+| after-error | Triggered after the server response fails. <br> Contains response payload.|
--- src/alv-form.vue
+++ src/alv-form.vue
@@ -1,33 +1,55 @@
 <template>
-    <form v-bind:action="action" method="post" @submit.prevent="sendFormData" ref="form">
+    <form v-bind:action="action" :method="method" @submit.prevent="sendFormData" ref="form">
         <input type="hidden" name="_method" :value="method">
-        <input type="hidden" name="csrf" :value="token">
         <slot></slot>
+
     </form>
 </template>
 
 <script>
+    import LoadingSpinner from "./lading-spinner"
+    import Repository from "./repository";
+
     export default {
         name: "alv-form",
-        data() {
-            return {
-                token: ''
-            }
-        },
         props: {
             action: String,
-            method: String,
+            method: {
+                type: String,
+                default: "post"
+            },
             resetOnDone: Boolean,
             dataObject: Object,
             dataArray: Object,
-            inputParentSelector: String
-        },
-        mounted() {
-            this.token = document.querySelector('[name="csrf-token"]').getAttribute('content');
+            inputParentSelector: {
+                type: String,
+                default: "div"
+            },
+            spinner: {
+                type: Boolean,
+                default: false
+            },
+            errorClass: {
+                type: String,
+                default: null
+            },
+            axiosConfig: {
+                type: Object,
+                default() {
+                    return {}
+                }
+            },
+            htmlErrors: {
+                type: Boolean,
+                default: false
+            },
+            focusableErrors: {
+                type: Boolean,
+                default: true
+            }
         },
         methods: {
             sendFormData() {
-                let $this = this;
                 let form = this.$refs.form;
                 let formData;
                 if (typeof this.dataObject !== "undefined")
@@ -36,17 +58,28 @@
                     formData = this.getFormArray();
                 else
                     formData = this.getFormNames(form);
+                this.setButtonLoading();
 
-                axios.post(this.action, formData)
-                    .then(response => {
-                        $this.$emit('after-done', response);
-                        if ($this.resetOnDone)
-                            form.reset();
-                        $this.dropAllErrors();
-                    }).catch(exception => {
-                    let errors = exception.response.data.errors;
-                    $this.showErrors(form, errors);
-                    $this.$emit('after-error');
+                window.axios({
+                    method: this.method.toLowerCase() == "get" ? "GET" : "POST",
+                    url: this.action,
+                    data: formData,
+                    ...this.axiosConfig
+                }).then(response => {
+                    this.$emit("after-done", response);
+                    if (this.resetOnDone)
+                        form.reset();
+                    this.dropAllErrors();
+                }).catch(exception => {
+                    this.$emit("after-error", exception.response.data);
+                    Repository.responseToJSON(exception.response.data).then(response => {
+                        this.unsetButtonLoading();
+                        let errors = response.errors;
+                        if (typeof errors == "object") {
+                            this.showErrors(form, errors);
+                            this.focusError();
+                        }
+                    });
                 });
             },
             getFormNames(form) {
@@ -60,81 +93,140 @@
             },
             getFormObject() {
                 let form = this.dataObject;
-                let formData = this.createFormData();
-                for (let name in form)
-                    if (form[name] instanceof FileList)
-                        for (let i = 0; i < form[name].length; i++)
-                            formData.append(name, form[name][i])
-                    else if (form[name] && typeof form[name] == "object")
-                        Object.keys(form[name]).forEach(objectName => {
-                            if (form[name][objectName] && typeof form[name][objectName] == "object")
-                                Object.keys(form[name][objectName]).forEach(objectObjectName => {
-                                    formData.append(`${name}[${objectName}][${objectObjectName}]`, form[name][objectName][objectObjectName]);
-                                });
-                            else
-                                formData.append(`${name}[${objectName}]`, form[name][objectName]);
-                        });
-                    else
-                        formData.append(name, form[name]);
-
+                var formData = this.createFormData();
+                Object.keys(form).forEach(key => {
+                    formData = this.append(formData, form, "", key)
+                });
                 return formData;
+            },
+            append(formData, form, name, key) {
+                if (form[key] instanceof Date) {
+                    formData.append(name ? name : key, form[key].toISOString());
+                    return formData;
+                } else if (form[key] instanceof FileList) {
+                    if (key.slice(-2) !== "[]") {
+                        formData.append(name ? name : key, form[key][0]);
+                        return formData;
+                    } else {
+                        for (let i = 0; i < form[key].length; i++)
+                            formData.append(name ? `${name}[]` : key, form[key][i]);
+                        return formData;
+                    }
+                } else if (form[key] && typeof form[key] == "object") {
+                    if (Array.isArray(form[key])) {
+                        form[key].forEach((item, index) => {
+                            formData = this.append(formData, form[key], name ? `${name}[${index}]` : `${key}[${index}]`, index)
+                        });
+                        return formData
+                    } else {
+                        Object.keys(form[key]).forEach(index => {
+                            let cleanIndex = index.slice(-2) === "[]" ? index.substring(0, index.length - 2) : index;
+                            formData = this.append(formData, form[key], name ? `${name}[${cleanIndex}]` : `${key}[${cleanIndex}]`, index)
+                        });
+                        return formData;
+                    }
+                } else {
+                    if (form[key] != null)
+                        formData.append(name ? name : key, form[key]);
+                    return formData;
+                }
             },
             createFormData() {
                 let formData = new FormData();
                 formData.append("_method", this.method);
-                formData.append("_token", this.token);
                 return formData;
             },
-
+            removeErrorInputsEvents() {
+                document.querySelectorAll(`.${this.errorClass} [name]`).forEach(input => {
+                        input.addEventListener("change", () => {
+                            input.closest(this.inputParentSelector).classList.remove(this.errorClass);
+                            input.closest(this.inputParentSelector).querySelector(".alv-error").remove();
+                        });
+                    }
+                )
+            },
             showErrors(form, errors) {
-                let $this = this;
                 let name_errors = Object.keys(errors);
-                this.getInputFormsNames().forEach(function (name) {
+                this.getInputFormsNames().forEach(name => {
                     let selector = "[name='" + name + "']";
-                    let formGroup = form.querySelector(selector).closest($this.parentSelector);
-                    let current_error = formGroup.getElementsByClassName('alv-error');
+                    let formGroup = form.querySelector(selector).closest(this.inputParentSelector);
+                    let current_error = formGroup.getElementsByClassName("alv-error");
                     if (name_errors.includes(name)) {
                         if (current_error.length > 0) {
-                            current_error[0].textContent = errors[name][0];
+                            current_error[0][this.htmlErrors ? "innerHTML" : "textContent"] = errors[name][0];
                         } else {
-                            let span = $this.getDefaultSpan();
-                            span.textContent = errors[name][0];
-                            formGroup.append(span)
+                            let span = this.getDefaultSpan();
+                            span[this.htmlErrors ? "innerHTML" : "textContent"] = errors[name][0];
+                            formGroup.append(span);
+                            if (this.errorClass != null) {
+                                formGroup.classList.add(this.errorClass);
+                            }
                         }
                     } else {
-                        if (current_error.length > 0)
+                        if (current_error.length > 0) {
                             current_error[0].remove();
+                        }
                     }
-
-                })
+                });
+                this.removeErrorInputsEvents();
             },
             dropAllErrors() {
-                let current_errors = document.getElementsByClassName('alv-error');
+                let current_errors = document.getElementsByClassName("alv-error");
                 while (current_errors[0]) {
                     current_errors[0].parentNode.removeChild(current_errors[0]);
                 }
             },
             getDefaultSpan() {
-                let span = document.createElement('span');
-                span.classList.add('alv-error');
-                span.style.fontSize = '70%';
-                span.style.color = 'crimson';
+                let span = document.createElement("span");
+                span.classList.add("alv-error");
+                span.style.fontSize = "70%";
+                span.style.color = "crimson";
                 return span;
             },
             getInputFormsNames() {
-                let inputForms = this.$refs.form.querySelectorAll(this.parentSelector + ' > [name]')
+                let inputForms = this.$refs.form.querySelectorAll(this.inputParentSelector + " [name]")
                 let names = []
                 inputForms.forEach(function (inputForm) {
-                    names.push(inputForm.getAttribute('name'));
+                    names.push(inputForm.getAttribute("name"));
                 });
                 return names
+            },
+            setButtonLoading() {
+                let button = this.submitButton;
+                if (button != null) {
+                    if (this.spinner) {
+                        const loading = new (window.Vue.extend(LoadingSpinner))();
+                        loading.$mount();
+                        button.appendChild(loading.$el);
+                    }
+                    button.disabled = true;
+                }
+            },
+            unsetButtonLoading() {
+                let button = this.submitButton;
+                if (button != null) {
+                    if (this.spinner) {
+                        button.lastChild.remove();
+                    }
+                    button.disabled = false;
+                }
+            },
+            focusError() {
+                let firstInputError = document.querySelector(".alv-error");
+                if (firstInputError) {
+                    firstInputError.closest(this.inputParentSelector).querySelector("input,select").focus();
+                }
             }
         },
         computed: {
-            parentSelector: function () {
-                return (typeof this.inputParentSelector !== "undefined") ? this.inputParentSelector : '.form-group';
+            id() {
+                return this.$refs.form.id;
+            },
+            submitButton() {
+                const button = this.$refs.form.querySelector("[type='submit']");
+                return button != null ? button : document.querySelector(`button[form="${this.id}"]`);
             }
-        }
+        },
     }
 </script>
 
